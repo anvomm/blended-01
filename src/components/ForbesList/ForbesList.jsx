31d7b19420496ecfe9b9b939f9f2f350a2716003
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-27 13:15:40 +0200

    deploy fix

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,5 +1,5 @@
 import { ForbesListItem } from 'components';
-import { Avatar } from 'components/BlogCard/BlogCard.styled';
+/* import { Avatar } from 'components/BlogCard/BlogCard.styled'; */
 import {
   BoardHeader,
   LeaderBoard,
@@ -10,25 +10,29 @@ import {
 } from './ForbesList.styled';
 
 export const ForbesList = ({ forbes }) => {
-  return <LeaderBoard>
-    <BoardHeader>
-      <BoardTitle>
-        <TitleTop>Forbes</TitleTop>
-        <TitleBottom>Leader board</TitleBottom>
-      </BoardTitle>
-    </BoardHeader>
+  return (
+    <LeaderBoard>
+      <BoardHeader>
+        <BoardTitle>
+          <TitleTop>Forbes</TitleTop>
+          <TitleBottom>Leader board</TitleBottom>
+        </BoardTitle>
+      </BoardHeader>
 
-    <LeaderBoardProfiles>
-      {forbes.map(({ id, name, capital, avatar, isIncrease }) => {
-        return <ForbesListItem
-          key={id}
-          name={name}
-          capital={capital}
-          avatar={avatar}
-          isIncrease={isIncrease}
-        />;
-      })}
-      {/* Довільна кіл-сть FriendListItem */}
-    </LeaderBoardProfiles>
-  </LeaderBoard>;
+      <LeaderBoardProfiles>
+        {forbes.map(({ id, name, capital, avatar, isIncrease }) => {
+          return (
+            <ForbesListItem
+              key={id}
+              name={name}
+              capital={capital}
+              avatar={avatar}
+              isIncrease={isIncrease}
+            />
+          );
+        })}
+        {/* Довільна кіл-сть FriendListItem */}
+      </LeaderBoardProfiles>
+    </LeaderBoard>
+  );
 };
